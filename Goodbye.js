163380238da41e7d8f869624fc3902322e321c37
bobiscrazycool <52102043+bobiscrazycool@users.noreply.github.com>
2019-06-30 10:57:26 -0400

    Update Goodbye.js

--- Goodbye.js
+++ Goodbye.js
@@ -1,8 +1,8 @@
-(function(window){
-var sayBye = {};
-var Word = "Good Bye";
-sayBye.speak = function (name) {
-  console.log(Word + " " + name);
-}
-window.byeSpeaker = byeSpeaker;
+(function(window) {
+    var byeSpeaker = new Object();
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.byeSpeaker = byeSpeaker;
 })(window);
